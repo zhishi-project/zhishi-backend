9fd19371ea8e5c45ad32ff10287b3d920644ce0d
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2016-01-15 11:42:06 +0100

    added omniauth config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :slack, ENV["SLACK_CLIENT_ID"], ENV["SLACK_CLIENT_SECRET"], scope: "client", team: "andela"
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+end
