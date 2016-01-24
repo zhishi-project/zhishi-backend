df2b183c8e9c4736409675d8963c1e77e80fd436
Oscar <osmond.oranagwa@andela.com>
2016-01-25 00:01:24 +0100

    Remove new and edit endpoints for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :questions do
+  resources :questions, except: [:new, :edit] do
     get "recent_answers"
     get "popular_answers"
-    resources :comments
-    resources :answers
+    resources :comments, except: [:new, :edit]
+    resources :answers, except: [:new, :edit]
   end
   get "questions/top_questions" => "questions#top_questions"
 
   resources :answers, except: [:index, :show, :create, :destroy, :update, :new, :edit] do
-    resources :comments
+    resources :comments, except: [:new, :edit]
   end
 
   resources :comments, only: [:update, :delete]
