04bbb7509b3d49e997e716eea0d6242805e14361
Osmond Oscar <osmond.oranagwa@andela.com>
2016-02-12 21:12:13 +0100

    * Update serializers to use UserSerializer attribute definition for serializing user object * Add user points to UserSerializer * Embed user objects in Question answers * Add default value, 0,  for user point in the db

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,12 +1,13 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :user, :content, :votes_count, :created_at, :updated_at, :comments_count
 
-
   def user
     {
       id: object.user.id,
       name: object.user.name,
-      image: object.user.social_providers.first.try(:profile_picture)
+      email: object.user.points,
+      image: object.user.social_providers.first.try(:profile_picture),
+      points: object.user.points
     }
   end
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,18 +1,10 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :title,:content, :user, :votes_count, :tags, :created_at, :updated_at, :answers_count, :comments_count, :views
-
-  def user
-    {
-      id: object.user.id,
-      name: object.user.name,
-      image: object.user.social_providers.first.try(:profile_picture)
-    }
-  end
+  attributes :id, :title,:content, :votes_count, :tags, :created_at, :updated_at, :answers_count, :comments_count, :views
+  belongs_to :user
 
   def attributes(*args)
-    data = super
-    data['answers'] = answers if instance_options[:include_answers]
-    data
+    class_eval { has_many :answers } if instance_options[:include_answers]
+    super
   end
 
   def answers
