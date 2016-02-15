138bdbbf6c9edf29497d511a2b2526cf7d9fa05b
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2016-02-15 11:57:41 +0100

    updated some methods and routings

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,13 +1,3 @@
-class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :user, :content, :votes_count, :created_at, :updated_at, :comments_count
-
-  def user
-    {
-      id: object.user.id,
-      name: object.user.name,
-      email: object.user.points,
-      image: object.user.social_providers.first.try(:profile_picture),
-      points: object.user.points
-    }
-  end
+class AnswerSerializer < MainSerializer
+  attributes :question_id, :created_at, :updated_at, :comments_count
 end
