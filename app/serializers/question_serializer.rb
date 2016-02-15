7f53482dd0f4205d4984e96f264a7723a06cb288
Osmond Oscar <osmond.oranagwa@andela.com>
2016-02-15 15:20:16 +0100

    Add comment relationship to Question and Answer serializers

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,7 +2,7 @@ class QuestionSerializer < MainSerializer
   attributes :title, :tags, :answers_count, :comments_count, :views
 
   def attributes(*args)
-    class_eval { has_many :answers } if instance_options[:include_answers]
+    class_eval { has_many :comments; has_many :answers } if instance_options[:include_answers]
     super
   end
 
