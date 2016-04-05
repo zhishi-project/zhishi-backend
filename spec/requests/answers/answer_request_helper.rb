220c17ec397eadcb14a36747312f090ab0da8dd0
Osmond Oscar <osmond.oranagwa@andela.com>
2016-04-05 19:12:44 +0100

    Update tests for the workers

--- spec/requests/answers/answer_request_helper.rb
+++ spec/requests/answers/answer_request_helper.rb
@@ -10,23 +10,3 @@ def path_helper(path, answer=false)
     send(path, question)
   end
 end
-
-
-=begin
-accept_question_answer(question, answer)
-question_answers(question)
-
-question_answer(question, answer)
-
-
-
-accept_question_answer POST   /questions/:question_id/answers/:id/accept(.:format) answers#accept
-      question_answers GET    /questions/:question_id/answers(.:format)            answers#index
-                       POST   /questions/:question_id/answers(.:format)            answers#create
-       question_answer GET    /questions/:question_id/answers/:id(.:format)        answers#show
-                       PATCH  /questions/:question_id/answers/:id(.:format)        answers#update
-                       PUT    /questions/:question_id/answers/:id(.:format)        answers#update
-                       DELETE /questions/:question_id/answers/:id(.:format)        answers#destroy
-
-
-=end
