c1d087b23a98b9b9601038fe63d4b40646f7cb56
Tobi Oduah <tobi.oduah@andela.com>
2016-05-25 16:39:11 +0100

    Tests for Comment Requests
    Wrote tests to test the requests being sent related to handling comments.
    Requests include those for creating a comment, updating a comment, or deleting a comment.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,4 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
+require "support/comment_shared_contexts"
 
 RSpec.describe CommentsController, type: :controller do
+  describe "#create" do
+    subject { build(:comment) }
+    let(:resource_name) { subject.comment_on_type.downcase.pluralize }
+    let(:resource_id) { subject.comment_on_id }
+
+    it "fails when resource id is invalid", valid_request: true do
+      post :create, resource_name: resource_name,
+                    resource_id: Faker::Number.between(-5, 0),
+                    content: subject.content
+
+      expect(response.status).to eq 404
+      expect(response).to match_response_schema('error/not_found')
+    end
+  end
 end
