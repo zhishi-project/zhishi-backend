e49d7af1ab88866e4039511b8a806e0cc2be12a4
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2016-05-27 06:14:51 +0100

    added a little helper to the migration

--- db/migrate/20160525060037_update_user_activity_record_for_current_resources.rb
+++ db/migrate/20160525060037_update_user_activity_record_for_current_resources.rb
@@ -1,10 +1,10 @@
 class UpdateUserActivityRecordForCurrentResources < ActiveRecord::Migration
   def up
-    [Question, Comment, Answer].each do |model_klass|
+    [Question, Answer, Comment].each do |model_klass|
       model_klass.includes(:activities).find_each do |resource|
         activities = resource.activities.pluck(:key)
         if activities.grep(/create|index/).empty?
-          resource.create_activity :create
+          resource.create_activity :create, created_at: resource.created_at, updated_at: resource.updated_at
         end
       end
     end
