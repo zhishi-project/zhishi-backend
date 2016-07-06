3e4fc96938ab3e186fccd306bf070972db651c7b
Osmond Oscar <osmond.oranagwa@andela.com>
2016-07-06 08:27:10 +0100

    add unit test for cookie_handler service

--- app/services/cookie_handler.rb
+++ app/services/cookie_handler.rb
@@ -9,26 +9,34 @@ class CookieHandler
       request.headers['Cookie'] = "#{COOKIE_KEY}=#{cookie}"
     end
 
-    begin
-      extract_user_and_token(response)
-    rescue JSON::ParserError => e
-      false
-    end
+    user_obj = confirm_response(response.body)
+    return unless user_obj
+
+    set_user_and_token(user_obj)
   end
 
   private
   def self.connect
-    @connection ||= Faraday.new(AUTH_URL) do |conn|
+    Faraday.new(AUTH_URL) do |conn|
       conn.request  :url_encoded             # form-encode POST params
       conn.response :logger                  # log requests to STDOUT
       conn.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
   end
 
-  def self.extract_user_and_token(response)
-    user = JSON.parse(response.body)
-    user = User.from_andela_auth(user)
-    token = TokenManager.generate_token(user.id)
-    [user, token]
+  def self.set_user_and_token(user_obj)
+    user = User.from_andela_auth(user_obj)
+    if user.id
+      token = TokenManager.generate_token(user.id)
+      [user, token]
+    end
+  end
+
+  def self.confirm_response(response)
+    begin
+      JSON.parse(response)
+    rescue JSON::ParserError => e
+      false
+    end
   end
 end
